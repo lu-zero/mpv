95832764b0dfb9c645f5f4ec82351cf4fc8ba5ae
Alexander Preisinger <alexander.preisinger@gmail.com>
2014-02-10 16:44:42 +0100

    wayland: follow new xdg changes
    This introduces 2 new features that we make use of. First is the delete event
    which tells the client that the compositor likes to close the window. This
    allows external clients to close mpv (like server side decorations).
    
    Second is the new configure event which removes the edges parameter. If the
    window was resized on the top left corner (as an example) then the compositor
    will move the window by itself on the next surface attach. This removes a lot
    of code, but there are now some glitches. The wayland/shm output works well but
    the osd is sometimes off and the wrong size. The wayland/opengl output is now
    pretty broken. I don't know why, but I hope it will resolve itself once the api
    is stable (I can guarantee that it won't resolve itself).
    
    EDIT: new changes were added to this rebase

--- video/out/vo_wayland.c
+++ video/out/vo_wayland.c
@@ -477,8 +477,6 @@ static bool resize(struct priv *p)
 {
     struct vo_wayland_state *wl = p->wl;
 
-    int32_t x = wl->window.sh_x;
-    int32_t y = wl->window.sh_y;
     wl->vo->dwidth = wl->window.sh_width;
     wl->vo->dheight = wl->window.sh_height;
 
@@ -492,12 +490,6 @@ static bool resize(struct priv *p)
                                             wl->window.height,
                                             p->dst_w,
                                             p->dst_h);
-
-    if (x != 0)
-        x = wl->window.width - p->dst_w;
-
-    if (y != 0)
-        y = wl->window.height - p->dst_h;
 
     mp_sws_set_from_cmdline(p->sws);
     p->sws->src = p->in_format;
@@ -532,8 +524,6 @@ static bool resize(struct priv *p)
         wl_region_destroy(opaque);
     }
 
-    p->x = x;
-    p->y = y;
     p->wl->window.events = 0;
     p->vo->want_redraw = true;
     return true;
@@ -563,7 +553,7 @@ static void frame_handle_redraw(void *data,
     struct buffer *buf = buffer_pool_get_front(&p->video_bufpool);
 
     if (buf) {
-        wl_surface_attach(wl->window.surface, buf->wlbuf, p->x, p->y);
+        wl_surface_attach(wl->window.surface, buf->wlbuf, 0, 0);
         wl_surface_damage(wl->window.surface, 0, 0, p->dst_w, p->dst_h);
 
         if (callback)
